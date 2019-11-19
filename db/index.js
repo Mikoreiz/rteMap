d89402b9f3a0fd84274d4ee80500601e7e3ac9aa
Mikhael Oreiro <mikhaeloreiro@yahoo.com>
2019-11-19 12:07:20 -0800

    Database connection added

--- db/index.js
+++ db/index.js
@@ -1,1 +1,25 @@
+const config = require("../config.json")
 const mysql = require("mysql")
+
+const mysqlConnection = mysql.createConnection({
+  host: config.mysqlConnectAuth.sqlHost,
+  user: config.mysqlConnectAuth.sqlUsername,
+  password: config.mysqlConnectAuth.sqlPassword,
+  port: config.mysqlConnectAuth.port,
+  database: config.mysqlConnectAuth.sqlDB
+})
+
+const makeDb = () => {
+  mysqlConnection.connect(err => {
+    if (!err) {
+      console.log("DB connected")
+    } else {
+      console.log(
+        "DB connection unsuccessful \n Error: " +
+          JSON.stringify(err, undefined, 2)
+      )
+    }
+  })
+}
+
+module.exports = makeDb
---
+++ route_info/index.js
@@ -0,0 +1,2 @@
+const DB = require("../db/index")
+// const database = DB.makeDb()
